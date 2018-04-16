13a7d75dd14ecd210f22bbebf55f8d73c5911aea
Nick Weinzapfel <weinzapf@gmail.com>
2018-04-15 21:21:42 -0400

    simple CMakeLists.txt and include json headers

--- source/main.cpp
+++ source/main.cpp
@@ -2,12 +2,20 @@
 #include <vector>
 #include <iostream>
 
+#include <nlohmann/json.hpp>
+
 using namespace std;
+using json = nlohmann::json;
 
 int main() {
     int status(0);
 
     cout << "hello world" << endl;
+
+    json j;
+    j["Name"] = "Nick";
+    j["age"] = 36;
+    cout << j << endl;
 
     return status;
 }
